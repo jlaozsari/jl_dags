8e60ffa05d6034eb0e732ec0678c3d9fe96be08b
jlaozsari <ahmet.ozsari@justlife.com>
2023-05-30 02:07:48 +0300

    update

--- dags/bigquery_workflow.py
+++ dags/bigquery_workflow.py
@@ -1,114 +1,28 @@
+import airflow
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.models.baseoperator import chain
-from airflow.utils.task_group import TaskGroup
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
-from google.cloud import bigquery
-from datetime import datetime
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(0),
+}
 
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
-from kubernetes.client import models as k8s_models
-from airflow.providers.google.cloud.operators.kubernetes_engine import (
-    GKECreateClusterOperator,
-    GKEDeleteClusterOperator,
-    GKEStartPodOperator,
-)
+dag = DAG('sampleDAG', default_args=default_args, schedule_interval=None)
 
 
-def build_tasks(table_list):
-    tasks = []
-    for table in table_list:
-        task_id = table + "_task"
-        table_type_ = table.split("_")[-1]
-        task = KubernetesPodOperator(
-                name=task_id,
-                image="debian",
-                cmds=["echo"],
-                arguments=[
-                    "--table", table,
-                    "--table_type", table_type_
-                ],
-                labels={"foo": "bar"},
-                task_id=task_id,
-                do_xcom_push=False
-        )
-    #     KubernetesPodOperator(
-    #     # The ID specified for the task.
-    #     task_id="pod-ex-minimum",
-    #     # Name of task you want to run, used to generate Pod ID.
-    #     name="pod-ex-minimum",
-    #     # Entrypoint of the container, if not specified the Docker container's
-    #     # entrypoint is used. The cmds parameter is templated.
-    #     cmds=["echo"],
-    #     # The namespace to run within Kubernetes, default namespace is
-    #     # `default`. In Composer 1 there is the potential for
-    #     # the resource starvation of Airflow workers and scheduler
-    #     # within the Cloud Composer environment,
-    #     # the recommended solution is to increase the amount of nodes in order
-    #     # to satisfy the computing requirements. Alternatively, launching pods
-    #     # into a custom namespace will stop fighting over resources,
-    #     # and using Composer 2 will mean the environment will autoscale.
-    #     namespace="default",
-    #     # Docker image specified. Defaults to hub.docker.com, but any fully
-    #     # qualified URLs will point to a custom repository. Supports private
-    #     # gcr.io images if the Composer Environment is under the same
-    #     # project-id as the gcr.io images and the service account that Composer
-    #     # uses has permission to access the Google Container Registry
-    #     # (the default service account has permission)
-    #     image="gcr.io/gcp-runtimes/ubuntu_18_0_4",
-    # )
-        tasks.append(task)
-    return tasks
+DriftTask = KubernetesPodOperator(
+    namespace='default',
+    image="python",
+    cmds=["python", "-c"],
+    arguments=["print('This code is running in a Kubernetes Pod')"],
+    labels={},
+    name="sampleDAG",
+    task_id="sampleDAG",
+    get_logs=True,
+    dag=dag,
+    log_events_on_failure=True,
+    is_delete_operator_pod=True)
 
-with DAG("bigquery_workflow", start_date=datetime(2022, 1, 1),
-         schedule_interval="20 01 * * *", catchup=False) as dag:
-    
-    # start = EmptyOperator(task_id="start")
 
-    ### eger marketing dm ve oncesi calisacak bir sql yazdiysak buraya cift tirnak icinde eklememiz yeterli oluyor. Otomatik olarak task tanimlanacaktir.
-    # table_list_staging = ["asofcalendar_dm","calendar_dm", "service_attribute_appointment_detail_stg","appointment_survey_dm", 
-    #                       "appointment_replacement_stg","availability_absenteesim_stg","appointment_pay_stg","appointment_request_stg",
-    #                       "credit_spent_stg","partner_booking_stg","previous_day_stg","previous_day_outbound_stg","service_attribute_stg",
-    #                       "rescheduled_appointments_detail_stg","checkout_reconciliation_stg","subscription_dm"]
-                           
-        
-    ### appointment_dm den sonra calisacak bir task varsa buraya ekleyebilirsin.
-    # horiz_list_one= ["policy_popup_reschedule_stg","driver_appointment_details_stg","marketing_stg","marketing_dm",
-    #                  "appointment_type_dm","appointment_stg","appointment_dm"]
-        
-    # ### cogs yapisinda bir ekleme yapmak istersen buraya ekleyebilirsin.
-    # table_list_cogs= ["voucher_dm", "cogs_dtc_stg","cogs_fixed_fee_ksa_stg","cogs_fixed_fee_stg","cogs_min_guar_success_stg"]
-        
-    # ### finance tablolarina bir tablo eklemek istersen buraya ekleyebilirsin.
-    # table_list_finance = ["cogs_distribution_stg" ,"cogs_distribution_to_gmv_stg","finance_dm" ,"finance_pl_weekly_data_etl_stg",
-    #                         "finance_pl_gmv_stg","finance_pl_distribution_stg","finance_pl_stg","finance_pl_dm","pl_new_format_stg"]
-
-    ### build task bu tasklari birbirine otomatik olarak baglayan bir task mekanizmasi kuracaktir. Bunu yapmasaydik 30-40 tablo icin 
-    ### tek tek task tanimlamamiz gerekecekti.
-    # task_staging = build_tasks(table_list_staging)
-    # with TaskGroup("staging_group") as staging_group:
-    #     task_staging = build_tasks(table_list_staging)
-    # with TaskGroup("first_horiz_group") as first_horiz_group:
-    #     tasks_horiz_one = build_tasks(horiz_list_one)
-    # with TaskGroup("cogs_group") as cogs_group:
-    #     tasks_cogs = build_tasks(table_list_cogs)
-    # with TaskGroup("finance_group") as finance_group:
-    #     tasks_finance = build_tasks(table_list_finance)
-
-    deneme = KubernetesPodOperator(
-                name="deneme",
-                image="debian",
-                cmds=["echo"],
-                labels={"foo": "bar"},
-                task_id="deneme",
-                do_xcom_push=False
-        )
-    # task_staging = build_tasks(table_list_staging)
-
-    
-    # finish = EmptyOperator(task_id="finish")
-
-    # chain(start, task_staging, *tasks_horiz_one, tasks_cogs, *tasks_finance, finish)
-    chain(deneme)
+DriftTask
